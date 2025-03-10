9c6b2545cbaa77695f8dcf6cf26c651295d2f32f
Hariprasath_M <143427323+Hariprasath76@users.noreply.github.com>
2025-03-10 10:41:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function showMessage() {
-    alert("Welcome to this unique website!");
-}
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
